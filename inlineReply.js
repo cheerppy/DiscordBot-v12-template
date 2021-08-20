c23b1ceab4cfc82732f2fee80a9243cadb663a5a
cheerppy <47920686+cheerppy@users.noreply.github.com>
2021-08-20 09:22:09 +0900

    Update inlineReply.js

--- inlineReply.js
+++ inlineReply.js
@@ -1,4 +1,4 @@
-const { APIMessage, Structures } = require("discord.js");
+const { APIMessage, Structures } = require("discord.js-12");
 
 class ExtAPIMessage extends APIMessage {
     resolveData() {
