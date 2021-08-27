60448607030ad962d1c419bbcd0d620bf33a62c0
cheerppy <47920686+cheerppy@users.noreply.github.com>
2021-08-27 11:31:16 +0900

    Update index.js

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const {
   promMsg, wait, collect, getJst, strJst, sort, dice 
 } = require("./module");
 
+var black = '\u001b[30m';var red = '\u001b[31m';var green = '\u001b[32m';var yellow = '\u001b[33m';var blue = '\u001b[34m';var magenta = '\u001b[35m';var cyan = '\u001b[36m';var white = '\u001b[37m';var reset = '\u001b[0m';
 const consolelog = console.log;console.log = (...a) => consolelog(strJst(getJst()), ...a);const consoleerror = console.error;console.error = (...a) => consoleerror(strJst(getJst()), ...a);
 
 const http = require('http'); const https = require("https");
@@ -36,7 +37,7 @@ const mainId = config.testId;
 http.createServer(function(req, res){if (req.method == 'POST'){var data = "";req.on('data', function(chunk){data += chunk;});req.on('end', function(){if(!data){console.log("No post data");res.end();return;}var dataObject = querystring.parse(data);console.log("post:" + dataObject.type);res.end();});}else if (req.method == 'GET'){res.writeHead(200, {'Content-Type': 'text/plain'});res.end(botName + ' is active now\ntest is ' + test + "\nvalidity is " + valid);}}).listen(3000);
 
 client.on('ready', message =>{
- console.log('Bot準備完了～');
+ consolelog(cyan + strJst(getJst()), 'Bot準備完了～', reset)
  if(test){
   client.user.setPresence({  activity: {name: 'test'}, status: "idle"  }); return;
  }
