98bb5aaf5407f1c0e3be71f46324de2346ddced8
cheerppy <47920686+cheerppy@users.noreply.github.com>
2021-08-27 19:18:08 +0900

    added gray

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const {
   promMsg, wait, collect, getJst, strJst, sort, dice 
 } = require("./module");
 
-var black = '\u001b[30m';var red = '\u001b[31m';var green = '\u001b[32m';var yellow = '\u001b[33m';var blue = '\u001b[34m';var magenta = '\u001b[35m';var cyan = '\u001b[36m';var white = '\u001b[37m';var reset = '\u001b[0m';
+var black = '\u001b[30m';var red = '\u001b[31m';var green = '\u001b[32m';var yellow = '\u001b[33m';var blue = '\u001b[34m';var magenta = '\u001b[35m';var cyan = '\u001b[36m';var white = '\u001b[37m';var gray = '\u001b[90m';var reset = '\u001b[0m';
 const consolelog = console.log;console.log = (...a) => consolelog(strJst(getJst()), ...a);const consoleerror = console.error;console.error = (...a) => consoleerror(strJst(getJst()), ...a);
 
 const http = require('http'); const https = require("https");
